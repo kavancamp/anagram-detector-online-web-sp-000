debb76df113dfe01694dc79717cec16a2c1e9bfa
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-03 01:06:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,14 @@ class Anagram
     @word = word
   end
 
-  def match 
+  def match
     (array_of_words)
 
     array_of_words.select do |element|
         (@word.split("").sort) == (element.split("").sort)
       end
     end
-    
-    
+
+
   end
 end
